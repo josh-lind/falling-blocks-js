f19acd764ab0bda0894b7aa784522a070f3b7472
josh-lind <lind.98@osu.edu>
2020-01-10 16:00:36 -0500

    more model functions

--- Model.js
+++ Model.js
@@ -2,19 +2,73 @@ const Model = function () {
     this.color = "rbg(0,0,0)";
     this.rgb = [0, 0, 0];
 
+    const blockSize = .1;
+
     this.myBlock = {
         x: .5,
         y: .5,
-        size: .1
+        size: blockSize
     };
 
     this.goodBlocks = [];
     this.badBlocks = [];
-    this.fallingSpeed = .02;
+    this.gravity = .02;
+    this.running = false;
+    this.score;
 
-    this.addBlock = (blockArray) => {
-        const 
-    };
+    const addFallingBlock = (blockArray) => {
+        const newBlock = {
+            x: Math.random() * (1 - blockSize),
+            y: -blockSize,
+            size: blockSize
+        }
+        blockArray.push(newBlock);
+    }
+
+    const moveFallingBlocks = () => {
+        this.goodBlocks.forEach(block => {
+            block.y = block.y + gravity;
+        });
+
+        this.badBlocks.forEach(block => {
+            block.y = block.y + gravity;
+        });
+    }
+
+    const deleteOffMapBlocks = () => {
+        this.goodBlocks = this.goodBlocks.filter(block => {
+            return block.y <= 1;
+        });
+        this.badBlocks = this.badBlocks.filter(block => {
+            return block.y <= 1;
+        });
+    }
+
+    const isIntersection = (b1, b2) => {
+        if (b1.x <= b2.x + b2.size && b1.x + b1.size >= b2.x) {
+            // At this point the two blocks are vertically aligned
+            if (b1.y <= b2.y + b2.size && b1.y + b1.size >= b2.y) {
+                // At this point the two blocks are vertically and horizontally aligned
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkAllBlocksForIntersect = () => {
+        const numGood = this.goodBlocks.length;
+        this.goodBlocks = this.goodBlocks.filter(b => !isIntersection(b, this.myBlock));
+        const numGoodDeleted = numGood - this.goodBlocks.length;
+        this.score += numGoodDeleted;
+
+        const numBad = this.badBlocks.length;
+        this.badBlocks = this.badBlocks.filter(b => !isIntersection(b, this.myBlock));
+        const numBadDeleted = numBad - this.badBlocks.length;
+        if (numBadDeleted > 0) {
+            // This means the player ran into a bad block. GAME OVER
+            this.running = false;
+        }
+    }
 
     this.update = () => {
         this.rbg[0] = (this.rgb[0] + 1) % 256;
